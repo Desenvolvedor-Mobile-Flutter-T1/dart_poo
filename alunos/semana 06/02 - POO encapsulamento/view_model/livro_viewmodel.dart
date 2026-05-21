2105941c40eeb59609c2ecb1e3ffac9869babe5d
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-05-20 21:34:04 -0300

    feat : Add LivroViewModel

--- alunos/semana 06/02 - POO encapsulamento/view_model/livro_viewmodel.dart
+++ alunos/semana 06/02 - POO encapsulamento/view_model/livro_viewmodel.dart
@@ -1,2 +1,11 @@
-// debe conseguir enxergar o repository 
-class LivroViewmodel {}
+// debe conseguir enxergar o repository
+import '../repositories/livro_repository.dart';
+
+class LivroViewmodel {
+  final data = LivroRepository().getRepository();
+  printData() => data.forEach(
+    (element) => print(
+      'TITLE : ${element.title} AUTOR : ${element.author} YEAR OF PUBLICATION: ${element.publicationAt} GENDER : ${element.gender} PAGES : ${element.pages}',
+    ),
+  );
+}
