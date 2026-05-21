9d3ea07953050c85dc93ef514388595f46380634
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-05-20 21:36:33 -0300

    implement LivroViewModel  in main

--- alunos/semana 06/02 - POO encapsulamento/view/main.dart
+++ alunos/semana 06/02 - POO encapsulamento/view/main.dart
@@ -1,3 +1,7 @@
 // so deve enxergar view model
 // deve conter todos os dados de um livro formatados de uma forma amigavel
-void main() {}
+import '../view_model/livro_viewmodel.dart';
+
+void main() {
+  LivroViewmodel().printData();
+}
