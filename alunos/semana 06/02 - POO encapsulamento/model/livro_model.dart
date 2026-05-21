0bc99018fdf03b0b12f0e44f63b603d9cb1e9cef
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-05-20 21:04:05 -0300

    feat : init LivroModel

--- alunos/semana 06/02 - POO encapsulamento/model/livro_model.dart
+++ alunos/semana 06/02 - POO encapsulamento/model/livro_model.dart
@@ -1,1 +1,14 @@
-class LivroModel {}
+class LivroModel {
+  String title;
+  String author;
+  int publicationAt;
+  String gender;
+  int pages;
+  LivroModel({
+    required this.title,
+    required this.author,
+    required this.publicationAt,
+    required this.gender,
+    required this.pages,
+  });
+}
